3231fc5a9be4a2fb8a78f388c0ffb1bc6d019df4
avaninath16 <avani.nath16@gmail.com>
2024-08-26 11:17:31 -0700

    WIPUpdate Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,51 @@
 //import stuff here
-
+import java.util.Scanner;
+import java.lang.Math.*;    
 //Your code here
+public class Program {
+    public static void main (String [] args){
+    Scanner myScanner = new Scanner (System.in);
+    int number1 = 0;
+    int number2 = 0;
+    
+    System.out.println("Enter number 1: ");
+    number1 = myScanner.nextInt();
+    System.out.println("Enter number 2: ");
+    number2 = myScanner.nextInt();
+    
+    int sum = number1 + number2;
+    System.out.println("The sum is " + sum);
+    
+    int difference = number1 - number2;
+    System.out.println("The difference is " + difference);
+    
+    
+    int product = number1*number2;
+    System.out.println("The product is " + product);
+    
+    double average = (number1+number2)/2;
+    System.out.println("The average is " + average);
+    
+    int absVal = Math.abs(difference);
+    System.out.println("The distance is " + absVal);
+    
+    if (number1 > number2) {
+        System.out.println("The maximum is " + number1);
+    }
+    else if (number2 > number1){
+        System.out.println("The maximum is " + number2);
+    }
+    else { 
+        System.out.print("No max or min");
+    }
+
+    
+    
+    
+    
+    
+}
+}
 
 //Paste console output below:
 /*
